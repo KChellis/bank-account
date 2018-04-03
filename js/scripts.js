8209a9a2d9e901a276e3222fd8739d4e828d11af
Cory West-Forbes and Kristen Chellis <kristen@>
2018-04-03 09:43:59 -0700

    create basic UI

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,15 @@ Account.prototype.withdraw = function (num) {
 Account.prototype.deposit = function (num) {
   return this.balance += num;
 };
+
+$(function(){
+  $('#acct-submit').submit(function(event){
+    event.preventDefault();
+    var userName = $('#name').val();
+    var userBalance = parseInt($('#balance').val());
+    var getBalance = new Account(userName, userBalance);
+
+    $('#balance').text(getBalance.balance);
+    $('.hidden').show();
+  });
+});
